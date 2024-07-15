1b2bf0d98edd509724777bf81180e1ecd8d6b7d2
tgunnr <finalresidentevilfantasy@gmail.com>
2024-07-14 23:31:42 -0700

    Exercise 3

--- app.js
+++ app.js
@@ -20,3 +20,16 @@ const game = {
 console.dir(pokemon, { maxArrayLength: null })
 
 console.log(game)
+
+/*
+Exercise 3
+1. Add a new property to the `game` object. Let's call it "difficulty".
+2. Choose a value for "difficulty" that you think fits the game. Ex: "Easy", "Med" or "Hard". How would you assign it?
+
+
+Solve Exercise 3 here:
+*/
+
+game["difficulty"] = ["story", "easy", "medium", "hard"]
+
+console.log(game)
